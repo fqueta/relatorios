06a74597ae5e8e790daaa01524ca53560167ebcf
relatorios <ger.maisaqui1@gmail.com>
2023-01-02 09:53:40 -0300

    2

--- app/Qlib/Qlib.php
+++ app/Qlib/Qlib.php
@@ -770,15 +770,16 @@ class Qlib
         $ano = date('Y');
         $mes = date('m');
         $mes = (int)$mes;
-        if($mes>8){
-            $ano = date('Y')+1;
-        }
         if($mes == 1){
             $mes = 12;
-            $ano = date('Y') - 1;
+            // $ano = date('Y') - 1;
         }else{
             $mes--;
         }
+        if($mes>8 && $mes!=12){
+            $ano = date('Y')+1;
+        }
+
         return ['ano'=>$ano,'mes'=>$mes];
     }
 }
